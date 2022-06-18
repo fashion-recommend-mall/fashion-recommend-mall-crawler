9f59d3c58c665792059bc74bf581ffab67befdab
gimseonjin <kimseonjin616@naver.com>
2022-06-18 19:49:53 +0900

    Build : Setting Celery in Get Items URL Service!
    Setting Celery in get_items_url_service to async
    
    참고 : #

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from selenium import webdriver
-from service.get_items_url_service import GetItemUrlService
+from service.get_items_url_service import get_items_url
 import json
 
 with open('targets.json') as f:
 	targets = json.load(f)
 
-getItemUrlService = GetItemUrlService()
-
 for target in targets["go_go_sing"]:
-    result = getItemUrlService.get_items_url(target)
+    result = get_items_url.delay(target)
     print(result)
--- service/get_items_url_service.py
+++ service/get_items_url_service.py
@@ -1,50 +1,68 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium_contextmanager import WebDriver
+from service.selenium_contextmanager import WebDriver
+import celery
+from celery import Task
 
-class GetItemUrlService():
+app = celery.Celery(
+    'get_item_urls',
+    broker="pyamqp://guest@localhost",
+    backend="rpc://guest@localhost"
+)
 
-    def _init_driver(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        options.add_argument('window-size=1920x1080')
-        options.add_argument("disable-gpu")
+def _init_driver():
 
-        driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
+    options = webdriver.ChromeOptions()
 
-        return driver
-    
-    def _valid_url(self, a_list : list[str]):
+    options.add_argument('headless')
 
-        temp_list = []
+    options.add_argument('window-size=1920x1080')
 
-        for a in a_list:
-            url = a.get_attribute("href")
-            if "javascript" in url :
-                url = url.split("javascript")[0]
-            if url != "":
-                temp_list.append(a.get_attribute("href").split("javascript")[0])
-                
-        return temp_list
+    options.add_argument("disable-gpu")
 
-    def get_items_url(self, state : dict):
+    driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
 
-        url_list = []
+    return driver
 
-        with WebDriver(self._init_driver()) as driver:
 
-            for page in range(1, state["pages"]+1):
+def _valid_url(a_list : list[str]):
 
-                driver.implicitly_wait(5)
+    temp_list = []
 
-                driver.get(state["base_url"] + f"&page={page}")
+    for a in a_list:
 
-                driver.implicitly_wait(5)
+        url = a.get_attribute("href")
 
-                a_list = driver.find_elements(By.XPATH, state["x_path"])
+        if "javascript" in url :
 
-                driver.implicitly_wait(5)
+            url = url.split("javascript")[0]
 
-                url_list.extend(self._valid_url(a_list))
+        if url != "":
 
-        return { state["category"] : url_list }
+            temp_list.append(a.get_attribute("href").split("javascript")[0])
+
+    return temp_list
+
+@app.task
+def get_items_url(state : dict):
+
+    url_list = []
+
+    with WebDriver(_init_driver()) as driver:
+
+        for page in range(1, state["pages"]+1):
+
+            driver.implicitly_wait(5)
+
+            driver.get(state["base_url"] + f"&page={page}")
+
+            driver.implicitly_wait(5)
+
+            a_list = driver.find_elements(By.XPATH, state["x_path"])
+
+            driver.implicitly_wait(5)
+
+            url_list.extend(_valid_url(a_list))
+
+    return { state["category"] : url_list }
+
