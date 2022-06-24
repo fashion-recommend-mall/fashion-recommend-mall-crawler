7a49827e3ac348ee6a72ea0cb92a2e0da2ce9a19
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-06-25 08:27:24 +0900

    Build : Add chromedriver path

--- src/robot.py
+++ src/robot.py
@@ -22,10 +22,5 @@ class Robot():
     def test(self):
         test = {'site': 'go_go_sing', 'category': 'Top', 'url': 'https://www.ggsing.com/product/%EC%BC%80%EC%9D%B4%EB%B8%94-%EB%B6%80%ED%81%B4-%EB%8B%88%ED%8A%B8%EC%A1%B0%EB%81%BC-%EB%B8%8C%EC%9D%B4%EB%84%A5%ED%81%AC%EB%A1%AD7%EA%B2%8C%EC%9D%B4%EC%A7%802%ED%95%A9%EA%B0%80%EB%91%98%EB%A0%88/58249/category/31/display/1/'}
 
-        layer1 = get_item_data(test)
-        print(layer1)
-        layer2 = get_keywords_from_reviews(layer1)
-        print(layer2)
-
-        get_sytle_from_img(layer2)
-        print("done")
+        result = get_item_data.s(test) | get_keywords_from_reviews.s() | get_sytle_from_img.s()
+        result()
