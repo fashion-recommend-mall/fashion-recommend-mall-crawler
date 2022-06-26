0440e7d5d3212e492e74dcd153a23ed56ef92e21
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-06-27 01:13:19 +0900

    Feature : Add Docstring In Robot

--- src/robot.py
+++ src/robot.py
@@ -1,3 +1,6 @@
+"""
+This module has robot class handling logics!
+"""
 from src.service.get_items_url_service import get_items_url
 from src.service.get_item_data_service import get_item_data
 from src.service.get_keywords_from_reviews_service import get_keywords_from_reviews
@@ -5,14 +8,37 @@ from src.service.get_style_from_img_service import get_sytle_from_img
 
 
 class Robot():
+    """
+    This is Robot Class
 
-    def __init__(self, states:dict):
+    This class is used to recognize to barcode
+
+    Attributes:
+        run : This Function is running crawling logics!
+    """
+    def __init__(self, states:dict) -> None:
+        """
+        This is Robot Constructure!
+
+        Args :
+            - states (dict) : Should has targets from target.json!
+        """
         self.states = states
         self.item_url_list : list[str] = []
 
 
-    def run(self):
+    def run(self) -> None:
+        """
+        This is run method
 
+        It will start crawling from 4 website
+
+        Args :
+            -
+
+        Returns :
+            -
+        """
         self.item_url_list = get_items_url(self.states)
 
         for url in self.item_url_list:
