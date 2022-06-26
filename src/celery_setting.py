30d36658bfb5e596b0361bb0bb7d5dcf845e655a
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-06-27 01:13:41 +0900

    Feature : Add Docstring In Celery Setting

--- src/celery_setting.py
+++ src/celery_setting.py
@@ -1,10 +1,22 @@
+"""
+This is celery files!
+
+If you want to create celery worker,
+
+Run this file with below code!
+
+- Celery -A src.celery_setting worker -l INFO
+"""
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
 
+from project_setting import celery_broker_url
+
+broker_url : str = celery_broker_url["celery_broker_url"]
 
 app = Celery(
     'src',
-    broker="pyamqp://guest@3.34.5.27",
+    broker=broker_url,
     include=['src.robot']
 )
 
