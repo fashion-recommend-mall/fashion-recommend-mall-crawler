83da2ca80f6c3ce52434e1d625b665096bffd609
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-06-27 11:06:18 +0900

    Refactoring : Add Docstring In App.py

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+"""
+This is Project Main Module
+
+It is made by carrykim
+
+linkedIn : linkedin.com/in/carrykim
+github : https://github.com/gimseonjin
+"""
 import json
 
 from src.robot import Robot
 
 
 def main():
+	"""
+	Title : main
+	
+	This is Project Main Method!!
+
+	made by carrykim
+	"""
 	with open('resource/targets.json') as f:
 		targets = json.load(f)
 	
